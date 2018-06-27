439412615d83825360eb74d23a2ab010f691e14c
ylc <yuanlinchengyx@163.com>
2018-06-27 08:39:17 +0800

    add application file

---
+++ src/main/java/com/ylc/Application.java
@@ -0,0 +1,14 @@
+package com.ylc;
+
+/**
+ * @author: tree
+ * @version: 1.0
+ * date: 2018/6/27 8:38
+ * @description: xxx
+ * own:
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("111111111111111111111");
+    }
+}
