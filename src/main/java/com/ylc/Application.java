1cce1f89fa30184957943c7008ff2e30de0f93d5
ylc <yuanlinchengyx@163.com>
2018-06-27 08:47:59 +0800

    change application file

--- src/main/java/com/ylc/Application.java
+++ src/main/java/com/ylc/Application.java
@@ -9,6 +9,6 @@ package com.ylc;
  */
 public class Application {
     public static void main(String[] args) {
-        System.out.println("111111111111111111111");
+        System.out.println("111111111111");
     }
 }
